7148f8e1b51093f09f725c2b337b3b271eaca06a
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-08 14:28:33 -0700

    push project structure

---
+++ src/main/java/ExampleClass.java
@@ -0,0 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class ExampleClass {
+    public static void main(String[] args) {
+        String adi = "Adi";
+        System.out.println("StingUtils.reverse(\"Adi\") " + StringUtils.reverse("adi"));
+    }
+}
