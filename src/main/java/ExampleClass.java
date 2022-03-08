a6c8a37b3294244ed0c7fff29bbf0712a8cc5324
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-08 14:28:44 -0700

    minor change

--- src/main/java/ExampleClass.java
+++ src/main/java/ExampleClass.java
@@ -3,6 +3,6 @@ import org.apache.commons.lang3.StringUtils;
 public class ExampleClass {
     public static void main(String[] args) {
         String adi = "Adi";
-        System.out.println("StingUtils.reverse(\"Adi\") " + StringUtils.reverse("adi"));
+        System.out.println("StingUtils.reverse(\"Adi\") " + StringUtils.reverse("Adi"));
     }
 }
