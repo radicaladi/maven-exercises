af4b386f2bf6245d49b7ea4fba45cd6d6e7198ac
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-08 15:13:44 -0700

    complete maven dependencies exercise

--- src/main/java/ExampleClass.java
+++ src/main/java/ExampleClass.java
@@ -1,8 +1,23 @@
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Scanner;
+
 public class ExampleClass {
     public static void main(String[] args) {
-        String adi = "Adi";
-        System.out.println("StingUtils.reverse(\"Adi\") " + StringUtils.reverse("Adi"));
+        Scanner scanned = new Scanner(System.in);
+        System.out.println("Enter something good (no numbers... or else): ");
+        String input = scanned.nextLine();
+        // checking for numeric inputs
+        if (!StringUtils.isAlpha(input)) {
+            System.out.printf("%s: I see some numbers there goofball.\n", input);
+        } else {
+            System.out.printf("%s: has no numbers! Good!\n", input);
+        }
+
+        // flips the case of string
+        System.out.println("Flipped case: " + StringUtils.swapCase(input));
+
+        // reverses the string
+        System.out.println("Reversed: " + StringUtils.reverse(input));
     }
 }
